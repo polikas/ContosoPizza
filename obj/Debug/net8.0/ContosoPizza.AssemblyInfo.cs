3909560971b0c4579f9a0c779c2c926527bc0b36
Stavros Charitos <polikasxaritos@gmail.com>
2024-08-08 17:55:05 +0100

    added Pizza controller

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c185ad35152a40b1310d01176408d842ce29314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0c649936036ece2fdbb53021ab457c2338aa7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
