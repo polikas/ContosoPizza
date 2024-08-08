1d0c649936036ece2fdbb53021ab457c2338aa7d
Stavros Charitos <polikasxaritos@gmail.com>
2024-08-08 17:28:29 +0100

    added model and service to build the api, still requires the controller though

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5879db4df93b1e1a56652e664df03c13764d00b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c185ad35152a40b1310d01176408d842ce29314")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
